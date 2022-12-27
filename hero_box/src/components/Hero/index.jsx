66921ee34b439587437acc2dc6e9f8dc8c135589
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-27 11:06:00 +0500

    Updated

---
+++ hero_box/src/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css"
+
+const Hero = () => {
+  return (
+    <div className='Hero'>
+            <div className="box_one"></div>
+            <div className="box_two"></div>
+    </div>
+  )
+}
+
+export default Hero
